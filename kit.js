c0f7ef80d3b9e8b380e27a870254b58d7dfdfefa
Divnoorpreet <77381888+Divnoorpreet@users.noreply.github.com>
2022-02-25 13:37:46 +0530

    Add files via upload

---
+++ kit.js
@@ -0,0 +1,32 @@
+img = "";
+status = "";
+
+function navi(){
+    window.location="index.html";
+}
+
+function preload(){
+    img = loadImage("kithen.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500, 300);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.dectect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
